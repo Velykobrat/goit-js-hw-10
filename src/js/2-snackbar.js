1584c70a3b6e3a06151a6e8626ab2712371a7b28
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-04-09 10:23:08 +0300

    Second task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,37 @@
-console.log("Snackbar");
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const delayInput = this.elements['delay'];
+  const stateInput = this.elements['state'];
+  const delay = parseInt(delayInput.value);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateInput.value === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise.then((delay) => {
+    iziToast.success({
+      title: 'Success',
+      message: `✅ Fulfilled promise in ${delay}ms`,
+    });
+  }).catch((delay) => {
+    iziToast.error({
+      title: 'Error',
+      message: `❌ Rejected promise in ${delay}ms`,
+    });
+  });
+
+  delayInput.value = '';
+});
